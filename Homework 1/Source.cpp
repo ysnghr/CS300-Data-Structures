25727e2f5441b7524d39e4be73b7d907871ae023
ysnghr <yasinugur.cs@gmail.com>
2021-11-01 01:28:15 +0300

    declared process struct

--- Homework 1/Source.cpp
+++ Homework 1/Source.cpp
@@ -5,6 +5,17 @@
 
 using namespace std;
 
+#define Queue(x) queue_ ## x
+
+struct Process {
+	vector<char> stages;
+	int index_current;
+	bool is_finished;
+
+	Process() : is_finished(false), index_current(0)
+	{}
+};
+
 string getFolderName() {
 	cout << "Please enter the process folder name: ";
 	string folderName, outputName = "output.txt";
@@ -16,7 +27,7 @@ string getFolderName() {
 }
 
 
-
 int main() {
 	string folderName = getFolderName();
+	queue<int> Queue(id);
 }
