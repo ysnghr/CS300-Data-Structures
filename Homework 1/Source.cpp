6c86a84a71e236bc0d39e2ed33fb074ba67d971e
ysnghr <yasinugur.cs@gmail.com>
2021-11-01 02:55:25 +0300

    files to vector of queue of structs

--- Homework 1/Source.cpp
+++ Homework 1/Source.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <sstream>
 #include <queue>
+#include <string>
 
 using namespace std;
 
-#define Queue(x) queue_ ## x
+//#define Queue(x) queue_ ## x
 
 struct Process {
-	vector<char> stages;
+	queue<char> stages;
 	int index_current;
 	bool is_finished;
 
-	Process() : is_finished(false), index_current(0)
+	Process() : is_finished(false), index_current(0), stages()
 	{}
 };
 
@@ -26,8 +27,8 @@ string getFolderName() {
 	return folderName;
 }
 
-string getConfigFilePath(const string &folderName) {
-	return folderName + "/" + "configuration.txt";
+string getFilePath(const string &folderName, string configFile) {
+	return folderName + "/" + configFile;
 }
 
 int readFromConfig(ifstream& configReader) {
@@ -36,13 +37,53 @@ int readFromConfig(ifstream& configReader) {
 	return value;
 }
 
+void readProcessFile(const string &processName, Process& newProcess) {
+	ifstream processFileReader(processName);
+	char processChar;
+	while (processFileReader >> processChar) {
+		newProcess.stages.push(processChar);
+	}
+	processFileReader.close();
+}
+
 int main() {
 	string folderName = getFolderName();
-	string configFilePath = getConfigFilePath(folderName);
+	string configFilePath = getFilePath(folderName, "configuration.txt");
 	ifstream configReader(configFilePath);
 	int numberOfQueues = readFromConfig(configReader), 
 		numberOfProcesses = readFromConfig(configReader), 
 		numberOfSlices = readFromConfig(configReader);
+	configReader.close();
+
+	vector<queue<Process>> allQueues;
+
+	for (int i = 0; i < numberOfQueues; i++) {
+		// queue<Process> Queue(id);
+		queue<Process> queueProcess;
+		allQueues.push_back(queueProcess);
+	}
+
+	for (int i = 1; i < numberOfProcesses + 1; i++)
+	{
+		string processFileName = "p" + to_string(i) + ".txt", processFilePath = getFilePath(folderName, processFileName);
+		Process newProcess;
+		readProcessFile(processFilePath, newProcess);
+		allQueues[0].push(newProcess);
+	}
+
+
+
+	// printing queues
+
+	//	while (!allQueues[0].empty()) {
+	//		while (!allQueues[0].front().stages.empty()) {
+	//			cout << allQueues[0].front().stages.front() << endl;
+	//			allQueues[0].front().stages.pop();
+	//		}
+	//		cout << "--------" << endl;
+	//		allQueues[0].pop();
+	//	}
+
 	
-	queue<int> Queue(id);
+	
 }
